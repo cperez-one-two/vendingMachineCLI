60496d2fcc0d59a581511d4f77acc28c8f2d9736
Chris Perez <christopher12perez6@gmail.com>
2020-10-08 17:10:30 -0400

    Inventory load method added

--- java/src/main/java/com/techelevator/VendingMachine.java
+++ java/src/main/java/com/techelevator/VendingMachine.java
@@ -1,11 +1,19 @@
 package com.techelevator;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class VendingMachine {
 	private Menu mainMenu;
 	private Menu purchaseMenu;
 	private boolean inPurchase;
 	private boolean running;
 	private int balance;
+	private Map<String, Product> slots = new HashMap<String, Product>();
+	private static final int MAX_STOCK = 5;
+	private static final String INVENTORY_FILENAME = "vendingmachine.csv";
 
 	public void exit() {running = false;}
 	public boolean isRunning(){return running;}
@@ -22,6 +30,7 @@ public class VendingMachine {
 				new SelectProductOperation(this),
 				new FinishTransactionOperation(this)});
 		balance = 0;
+		loadInventory(INVENTORY_FILENAME);
 	}
 	
 	
@@ -62,5 +71,37 @@ public class VendingMachine {
 	public void endPurchase(){
 		inPurchase = false;
 		balance = 0;
+	}
+	
+	private void loadInventory(String filename) {
+		
+		File inventoryFile = new File(filename);
+		try (Scanner scanner = new Scanner(inventoryFile)) {
+			while(scanner.hasNext()) {
+				String currentLine = scanner.nextLine();
+				String[] params = currentLine.split("\\|");
+				int parsedPrice = Integer.parseInt(params[2].replace(".", ""));
+				switch(params[3]) {
+					case "Chip":
+						slots.put(params[0], new Chips(params[1], parsedPrice, MAX_STOCK));
+						break;
+					case "Candy":
+						slots.put(params[0], new Candy(params[1], parsedPrice, MAX_STOCK));
+						break;
+					case "Gum":
+						slots.put(params[0], new Gum(params[1], parsedPrice, MAX_STOCK));
+						break;
+					case "Beverage":
+						slots.put(params[0], new Beverage(params[1], parsedPrice, MAX_STOCK));
+						break;
+					default:
+						throw new Exception("Something wrong with input file.");
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println("Error loading inventory.");
+			System.exit(1);
+		}
 	}
 }
